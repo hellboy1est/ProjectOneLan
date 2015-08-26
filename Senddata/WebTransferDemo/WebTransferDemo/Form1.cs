12048e897f5009a5fedcdf76153c8645d12073a3
hellboy1est <rahul.kakkar69@yahoo.com>
2015-08-27 03:04:57 +1200

    First try to connect with mysql,error

--- Senddata/WebTransferDemo/WebTransferDemo/Form1.cs
+++ Senddata/WebTransferDemo/WebTransferDemo/Form1.cs
@@ -1,19 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
 
 namespace WebTransferDemo
 {
     public partial class Form1 : Form
     {
+        WebClient client = new WebClient();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connection = "server=127.0.0.1; database=getaddress;user=root; password=;";
+                MySqlConnection conn = new MySqlConnection(connection);
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+ 
         }
     }
 }
