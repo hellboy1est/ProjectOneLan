8e5ee4d6133d9a048993044701e87f2572e78465
hellboy1est <rahul.kakkar69@yahoo.com>
2015-09-04 23:47:47 +1200

    fixed,saves the state of device when scanning again

--- BluetoothTutorial/BluetoothTutorial/Form1.cs
+++ BluetoothTutorial/BluetoothTutorial/Form1.cs
@@ -140,8 +140,7 @@ namespace BluetoothTutorial
 
         private void mainProgram()
         {
-          
-
+           
             //Auto run method
             if (radioButton1.Checked == true)
             {
@@ -289,44 +288,6 @@ namespace BluetoothTutorial
             
             MessageBox.Show(webData);
 
-        }
-
-         private void ds()
-        {
-            // Create a request using a URL that can receive a post. 
-            WebRequest request = WebRequest.Create("http://kate.ict.op.ac.nz/~kakkr1/Assignment2/homePage.php ");
-            // Set the Method property of the request to POST.
-            request.Method = "POST";
-            // Create POST data and convert it to a byte array.
-            string postData = "This is a test that posts this string to a Web server.";
-            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
-            // Set the ContentType property of the WebRequest.
-            request.ContentType = "application/x-www-form-urlencoded";
-            // Set the ContentLength property of the WebRequest.
-            request.ContentLength = byteArray.Length;
-            // Get the request stream.
-            Stream dataStream = request.GetRequestStream();
-            // Write the data to the request stream.
-            dataStream.Write(byteArray, 0, byteArray.Length);
-            // Close the Stream objectx .
-            dataStream.Close();
-            // Get the response.
-            WebResponse response = request.GetResponse();
-            // Display the status.
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-            // Get the stream containing content returned by the server.
-            dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd();
-            // Display the content.
-            Console.WriteLine(responseFromServer);
-            MessageBox.Show(responseFromServer);
-            // Clean up the streams.
-            reader.Close();
-            dataStream.Close();
-            response.Close();
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
