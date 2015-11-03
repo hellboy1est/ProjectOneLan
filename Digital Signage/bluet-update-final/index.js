58f5c74ed5779d6ce4690f9e3f76612da06ad3ec
hellboy1est <rahul.kakkar69@yahoo.com>
2015-11-03 21:50:35 +1300

    final version of Digital Signae aapplication

--- Digital Signage/bluet-update-final/index.js
+++ Digital Signage/bluet-update-final/index.js
@@ -1,31 +1,19 @@
-//var app = require('http').createServer(handler)
-//var io = require('socket.io')(app);
-//var fs = require('fs');
-//require module
-//var Scanner = require("bluetooth-scanner");
-// var a='';
- 
-// define input
-//var device = "hci0";
-//var mac=1;
-//app.listen(80);
+/**
+Socket client(index.js) => Runs bluetooth scanner and sends devices to server.js
+**/ 
 
 var port = 80,
     devices = "hci0",
     socket = require('socket.io-client')('http://localhost:'+port);
     Scanner = require("bluetooth-scanner");
- 
- 
-
+  
 function main()
 {
     var bleScanner = new Scanner(devices);
     bleScanner.on("device",function(mac, name) {
      	console.log('Found device: ', name, ' with MAC: ', mac);
     	//var message = {'address': mac, 'name': name}; // prepare message
-			socket.emit('add-device', mac, name);
-    	//sendMessage('add-device', mac,name); // actually send message to 		server
-        //console.log("Sent new device", message);
+			socket.emit('add-device', mac, name); 
 	
     }); 
     bleScanner.on("done", function(msg){
